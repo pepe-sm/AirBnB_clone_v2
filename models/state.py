92991f6bc814d8ed9e6d4002ea662076e741baa1
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-09-20 03:39:46 -0700

    fixed model package, also corrected storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """Getter"""
+            from models import storage
+            from models.city import City
+            all_cities = []
+            for city_obj in storage.all(City).values():
+                # key = key.split(".")[0]
+                # if key == "City" and city_obj.state_id == self.id:
+                if city_obj.state_id == self.id:
+                    all_cities.append(city_obj)
+            return all_cities
