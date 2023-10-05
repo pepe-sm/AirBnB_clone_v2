b3de9b406878f1166d1cf1a73b192a88f1d1e0cf
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-10-05 15:49:37 -0700

    errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,25 +24,23 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """function2"""
-    token_path = archive_path.split("/")
-    path1 = token_path[1]
-    token_path2 = token_path[1].split(".")
-    path2 = token_path2[0]
-    if path.exists(archive_path):
+    """deploy to server"""
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p  /data/web_static/releases/{}/".format(path2))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(path1, path2))
-        run("sudo rm /tmp/{}".format(path1))
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}/".format(path2, path2))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(path2))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ \
-/data/web_static/current".format(path2))
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
         print("New version deployed!")
         return True
-    else:
+    except Exception:
         return False
