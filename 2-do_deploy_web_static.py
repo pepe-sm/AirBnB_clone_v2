ea5a776f8df8454f4a4724e6bd57337a42b896bc
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-10-10 06:15:59 -0700

    servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import os
 import os.path as path
 
-env.hosts = ['35.237.217.126', '35.229.75.225']
+env.hosts = ['35.175.64.173', '54.158.205.17']
 now = datetime.now()
 dt_string = now.strftime("%Y%m%d%H%M%S")
 
